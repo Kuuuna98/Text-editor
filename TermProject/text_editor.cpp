c8c17a5e8506f7f92357958cf7758c952acfe5c1
Kuuuna98 <yunaghgh@naver.com>
2020-12-05 17:59:03 +0900

    초기 커밋.

---
+++ TermProject/text_editor.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+
+int main() {
+	
+	std::string text_file_name = "test.txt";
+
+	std::ifstream in(text_file_name);
+
+	if (in.is_open())
+	{
+		std::string text_temp; //변수명 변경
+		getline(in, text_temp);
+		std::cout << text_temp << std::endl;
+	}
+	else std::cout << "Unable to open file";
+	in.close();
+
+	return 0;
+}
