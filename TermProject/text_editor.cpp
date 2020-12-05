640dacdef6861c7019a3065e33013629a6aa3a6c
Kuuuna98 <yunaghgh@naver.com>
2020-12-05 18:02:36 +0900

    ✨insert in vector after split text

--- TermProject/text_editor.cpp
+++ TermProject/text_editor.cpp
@@ -9,15 +9,22 @@
 
 int main() {
 	
+	std::string stringBuffer;
 	std::string text_file_name = "test.txt";
+	std::vector<std::string> text;
 
 	std::ifstream in(text_file_name);
 
+	text.clear();
 	if (in.is_open())
 	{
 		std::string text_temp; //변수명 변경
 		getline(in, text_temp);
-		std::cout << text_temp << std::endl;
+		std::istringstream ss(text_temp);
+
+		while (getline(ss, stringBuffer, ' ')) {
+			text.push_back(stringBuffer);
+		}
 	}
 	else std::cout << "Unable to open file";
 	in.close();
