60d661ecd0f42046834a27f71a7181572e160bd4
Kuuuna98 <yunaghgh@naver.com>
2020-12-06 23:24:41 +0900

    🎨Implemented TextBook class

--- TermProject/text_editor.cpp
+++ TermProject/text_editor.cpp
@@ -6,28 +6,44 @@
 #include <string>
 #include <sstream>
 
-
-int main() {
-	
+class TextBook {
+private:
 	std::string stringBuffer;
-	std::string text_file_name = "test.txt";
+	std::string text_file_name;
+public:
 	std::vector<std::string> text;
 
-	std::ifstream in(text_file_name);
+public:
 
-	text.clear();
-	if (in.is_open())
-	{
-		std::string text_temp; //변수명 변경
-		getline(in, text_temp);
-		std::istringstream ss(text_temp);
-
-		while (getline(ss, stringBuffer, ' ')) {
-			text.push_back(stringBuffer);
-		}
+	TextBook(std::string text_file_name) {
+		this->text_file_name = text_file_name;
+		readFile();
 	}
-	else std::cout << "Unable to open file";
-	in.close();
+private:
+	void readFile() {
+		std::ifstream in(text_file_name);
+
+		text.clear();
+		if (in.is_open())
+		{
+			std::string text_temp; //변수명 변경
+			getline(in, text_temp);
+			std::istringstream ss(text_temp);
+
+			while (getline(ss, stringBuffer, ' ')) {
+				text.push_back(stringBuffer);
+			}
+		}
+		else std::cout << "Unable to open file";
+		in.close();
+	}
+
+};
+
+
+int main() {
+	TextBook* book = new TextBook("test.txt");
+	
 
 	return 0;
 }
